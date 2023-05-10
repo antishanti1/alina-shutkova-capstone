78d4e80ecf94cf6706c99672a04564adb81227e6
antishanti1 <shutkova2603@gmail.com>
2023-05-10 16:54:49 -0400

    updated markers

--- src/pages/Map/MainMap.js
+++ src/pages/Map/MainMap.js
@@ -2,12 +2,17 @@ import React, {useEffect, useState} from 'react';
 import ReactMapGL, { Marker , Popup} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 // import  testData from '../../testData/test.json';
-import { FcLike } from "react-icons/fc";
+import { FaMapMarkerAlt } from "react-icons/fa";
 import axios from 'axios';
 
 
 export default function MainMap () {
+    const currentUser = "jerry";
     const [listings, setListings] = useState([]);
+    const [popupOpen, setPopupOpen] = useState(false);
+    const [selectedList, setSelectedList] = useState(null);
+    const [newListing, setNewListing] = useState(null);
+
     const [viewport, setViewport] = useState({
         latitude: 25.7741728,
         longitude: -80.19362,
@@ -15,67 +20,104 @@ export default function MainMap () {
         height: "100vh",
         zoom: 12,
     })
-
-    // const [selectedList, setSelectedList] = useState(null);
+  
 
     useEffect(() => {
      const getListings = async () => {
         try {
-            // const response = await axios.get('http://localhost:5050/api/listings');    
-            const response = await axios.get('/listings');    
+            const response = await axios.get('http://localhost:5050/api/listings');      
             setListings(response.data);    
      }catch (error) {
-         console.error(error);
+         console.log(error);
      }
     };
     getListings();
-    },[])
+    },[]);
 
 
-
+    const handleAddClick = (e) => {
+        const { lng, lat } = e.lngLat;
+        setNewListing({
+            lng: lng,
+            lat: lat,
+        });
+    }
+    
+    
     return (
  
     <div style={{width: "100%", height: "100%"}}>
         <ReactMapGL 
          {...viewport}
         mapboxAccessToken={"pk.eyJ1IjoiYXNodXRrb3ZhIiwiYSI6ImNsaGdvMWllbzA2cWUzbG56cHd0OWNiMXIifQ.OY3xKwqI9zbx6aflRvdEtw"}
-       
         onViewportChange={newViewport => {
          setViewport(newViewport);
             }}
             mapStyle="mapbox://styles/ashutkova/clhgnu8xz000i01qtdusxcett"
->
-{listings.map((listing) => (
-    <Marker key={listing.id} 
-    latitude={listing.latitude} 
-    longitude={listing.longitude}>
+     onDblClick={handleAddClick}
 
-       <button className="marker-btn" onClick={(e) => { 
-              e.preventDefault();
-              setListings(listing);
-       }}
-      > 
-       <FcLike />
-        </button>
+>
+{listings.map( listing => (
+    < React.Fragment key={listing._id}>
+    <Marker 
+    latitude={listing.lat} 
+    longitude={listing.long}
+    onClick={(values) => {
+        setPopupOpen({[listing._id]: true})
+        setSelectedList(listing);
+    }}
+    >
+
+       <FaMapMarkerAlt  
+         style={{
+         fill: listing.username === currentUser ? "yellow" : "blue",
+         fontSize: viewport.zoom * 3,
+         cursor: "pointer",}}
+       />
+       
     </Marker>
 
-        //   <Popup
-        //     latitude={listing.latitude}
-        //     longitude={selectedList.longitude}
-        //     onClose={() => setSelectedList(null)}
-        //     closeOnClick={false}
-        //     anchor="top"
-        //   >
-        //     <div>
-        //         <p>Title: {selectedList.title}</p>
-        //         <p>Description: {selectedList.description}</p>
-        //         <p>Quantity: {selectedList.quantity}</p>
-        //         <p>Phone number: {selectedList.phone_number}</p>
-        //         <p>Email: {selectedList.email}</p>
-        //     </div>
-        //   </Popup>
+    {popupOpen[listing._id] && (
+          <Popup
+            latitude={selectedList?.lat}
+            longitude={selectedList?.long}
+            onClose={() => {
+                setSelectedList(null)
+                setPopupOpen(false)
+            }}
+            closeOnClick={false}
+            
+            anchor="top"
+          >
+            <div>
+                <p> {selectedList?.username}</p>
+                <p>Title: {selectedList?.title}</p>
+                <p>Description: {selectedList?.description}</p>
+                <p>Quantity: {selectedList?.quantity}</p>
+                <p>Phone number: {selectedList?.phone_number}</p>
+                <p>Email: {selectedList?.email}</p>
+            </div>
+          </Popup>
+        )}
+{ newListing && (
 
 
+<Popup
+            latitude={newListing.lat}
+            longitude={newListing.lng}
+            onClose={() => {
+                setSelectedList(null)
+                setPopupOpen(false)
+                setNewListing(null) 
+            }}
+            closeOnClick={false}
+            
+            anchor="top"
+          >test</Popup>
+)}
+
+
+</React.Fragment>
 ))}
 
 
